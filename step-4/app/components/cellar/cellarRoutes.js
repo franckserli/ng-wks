fdce2d2822f5a3a95c048abc48d300ff43994d99
Franck Michaud <franckuser16@gmail.com>
2016-05-20 15:30:08 +0200

    update step-4

--- step-4/app/components/cellar/cellarRoutes.js
+++ step-4/app/components/cellar/cellarRoutes.js
@@ -1,27 +1,6 @@
 export default function cellarRouting($stateProvider, $urlRouterProvider) {
   $urlRouterProvider
     .otherwise('/cards');
-
-  $stateProvider
-  .state('cellar.cards', {
-    url: '/cards',
-    template: '<wine-cards wines="wines" with-input="true"></wine-cards>',
-    controller: ($scope, wines) => Object.assign($scope, { wines }),
-    resolve: {
-      WineService: 'WineService',
-      wines: ['WineService', (WineService) => WineService.loadRated()],
-    },
-  })
-  .state('cellar.edit', {
-    url: '/card/:id',
-    template: '<wine-edit wine="wines[0]" save="$ctrl.save(wines[0])"></wine-edit>',
-    controller: ($scope, wines) => Object.assign($scope, { wines }),
-    resolve: {
-      WineService: 'WineService',
-      wines: ['WineService', '$stateParams', (WineService, $stateParams) =>
-        WineService.load($stateParams.id)],
-    },
-  });
 }
 
 cellarRouting.$inject = ['$stateProvider', '$urlRouterProvider'];
--- step-4/app/components/discover/discoverRoutes.js
+++ step-4/app/components/discover/discoverRoutes.js
@@ -1,17 +1,6 @@
 export default function discoverRouting($stateProvider, $urlRouterProvider) {
   $urlRouterProvider
     .otherwise('/cards');
-
-  $stateProvider
-  .state('discover.cards', {
-    url: '/cards',
-    template: '<wine-cards wines="wines" with-input="false"></wine-cards>',
-    controller: ($scope, wines) => Object.assign($scope, { wines }),
-    resolve: {
-      WineService: 'WineService',
-      wines: ['WineService', (WineService) => WineService.loadAll()],
-    },
-  });
 }
 
 discoverRouting.$inject = ['$stateProvider', '$urlRouterProvider'];
