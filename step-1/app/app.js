faa9907e04505edbfaac28104cf889e5aa2cc7da
Franck Michaud <franckuser16@gmail.com>
2017-02-08 09:48:43 +0100

    step-1

---
+++ step-1/app/app.js
@@ -0,0 +1,5 @@
+import angular from 'angular';
+import ngAnimate from 'angular-animate';
+import ngMessageFormat from 'angular-message-format';
+
+export default angular.module('wine', [ngAnimate, ngMessageFormat]);
